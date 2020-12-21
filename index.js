5a652a4e1d8ab27502bbb504ffe9c68b3d73a58a
makrag <marka.gergo@tmx.hu>
2020-12-21 15:10:10 +0100

    alapok elkészítése

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
+'use strict';
 require('dotenv').config();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const {Client,TextChannel, Guild, } = require('discord.js');
+var dcBot = {
+  token: process.env.TOKEN,
+  server: process.env.SERVERNAME,
+  channelName: process.env.CHANNEL,
+  bot: new Client(),
+  interFace: null,
+  channel: null,
+  login: function () {
+    this.bot.login(this.token);
+  },
+  init: function(){
+    this.bot.on('ready', () => {
+      //console.info(`Logged in as ${bot.user.tag}!`);
+      this.getInterFace().then(function () {
+        dcBot.bindUI();
+      });
 
-bot.login(TOKEN);
+    })
+  },
+  bindUI: function () {
+    this.bot.on('message', (message) => {
+        if(message.content.substring(0,4) == '!msg'){
+          dcBot.Response(message.content.substring(5));
+        }
+    });
+  },
+  getInterFace: async function () {
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
-});
+    var channels = await this.bot.channels.array();
+    for (let i = 0; i < channels.length; i++) {
+      var channel = channels[i];
+      if(channel.guild.name == dcBot.server && channel.name == dcBot.channelName){
+        dcBot.channel = channel;
+        await channel.fetchMessages({ limit: 1 }).then(messages => {
+          dcBot.interFace = messages.first();
+        }).catch(console.error);
+      }
+    }
+  },
+  Response: function (msg) {
+    if(dcBot.interFace == undefined){
+        this.channel.send('Nincs üzenet.').then(message => dcBot.interFace = message)
+            .catch(console.error);
+    }else {
+      dcBot.interFace.edit(msg);
+      dcBot.interFace.reply();
+      var channels = this.bot.channels.array();
+      for (let i = 0; i < channels.length; i++) {
+        var channel = channels[i];
+        if (channel.guild.name == dcBot.server && channel.name == dcBot.channelName) {
+          channel.fetchMessages({limit: 1}).then(messages => {
+            messages.first().delete(0).then(function () {
 
-bot.on('message', msg => {
-  if (msg.content === '/ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+            }).catch(console.error);
+          }).catch(console.error);
+        }
+      }
     }
   }
-});
+};
+
+dcBot.login();
+dcBot.init();
